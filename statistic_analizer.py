c1e408e20d3795d404a8de7bf49afed63fcdc097
lyhernkeith <68096948+lyhernkeith@users.noreply.github.com>
2025-12-17 19:47:49 +0800

    bettervisuals

--- statistic_analizer.py
+++ statistic_analizer.py
@@ -258,14 +258,18 @@ window.onload = function() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.circleMarker([lat, lon], {
-        radius: 8,
-        color: "red",
-        fillColor: "#f03",
-        fillOpacity: 0.8
-    })
-    .addTo(map)
-    .bindPopup("Current Location");
+    var redIcon = L.icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+        iconSize: [25, 41],   
+        iconAnchor: [12, 41], 
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]  
+    });
+
+    var marker = L.marker([4.377986, 113.977302], { icon: redIcon })
+        .addTo(map)
+        .bindPopup("Current Location");
 
 }
 </script>
