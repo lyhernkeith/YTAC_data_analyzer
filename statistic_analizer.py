c45ed5688edd65ac6882253a39f2aff2c779026e
lyhernkeith <68096948+lyhernkeith@users.noreply.github.com>
2026-01-07 15:12:53 +0800

    wretrthgyjh

--- statistic_analizer.py
+++ statistic_analizer.py
@@ -329,23 +329,13 @@ def get_highest_total_row():
         )
         |> map(fn: (r) => ({{
             r with
-
-            ph_pct:
-                (abs(r.PH - 7.0) / 7.0) * 100.0,
-
-
-            temp_pct:
-                abs(r.Temperature - 25.0) / 20.0 * 100.0,
-
-            turb_pct:
-                if r.Turbidity <= 0.0 then 0.0
-                else (r.Turbidity / 200.0) * 100.0,
-                
+            ph_pct: (abs(r.PH - 7.0) / 7.0) * 100.0,
+            temp_pct: abs(r.Temperature - 25.0) / 20.0 * 100.0,
+            turb_pct: if r.Turbidity <= 0.0 then 0.0 else (r.Turbidity / 200.0) * 100.0
         }}))
         |> map(fn: (r) => ({{
             r with
-            pollution_score:
-                (r.ph_pct + r.temp_pct + r.turb_pct) / 3.0
+            pollution_score: (r.ph_pct + r.temp_pct + r.turb_pct) / 3.0
         }}))
         |> sort(columns: ["pollution_score"], desc: true)
         |> limit(n: 1)
